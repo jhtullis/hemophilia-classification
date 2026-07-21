ed391e5b05a0c1c4809b5b9a346d8aeb6194a0d2
Henry Tullis <jhtullis@login01.rc.byu.edu>
2026-07-21 12:50:35 -0600

    chore: cut down requested memory

--- slurm/train_mpatch_v1_full_a.sh
+++ slurm/train_mpatch_v1_full_a.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=fibrin_mpatch_v1_full_a
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=120G
+#SBATCH --mem=70G
 #SBATCH --partition=eng,m13h,mgh
 #SBATCH --gres=gpu:1
 #SBATCH --time=12:00:00
